4eaf06d18640c7a87258e5aaf55792b52a67d843
Jason <jasongraham8231@gmail.com>
2024-02-05 18:19:50 -0500

    Add files via upload

---
+++ Tetris/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.tmrpiece = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrfastdown = New System.Windows.Forms.Timer(Me.components)
+        Me.btnreplay = New System.Windows.Forms.Button()
+        Me.lblnext = New System.Windows.Forms.Label()
+        Me.lblcredit = New System.Windows.Forms.Label()
+        Me.btnstart = New System.Windows.Forms.Button()
+        Me.pcttitle = New System.Windows.Forms.PictureBox()
+        Me.pctnext = New System.Windows.Forms.PictureBox()
+        Me.lblscore = New System.Windows.Forms.Label()
+        CType(Me.pcttitle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pctnext, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrpiece
+        '
+        Me.tmrpiece.Interval = 500
+        '
+        'tmrfastdown
+        '
+        Me.tmrfastdown.Interval = 50
+        '
+        'btnreplay
+        '
+        Me.btnreplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnreplay.Location = New System.Drawing.Point(266, 83)
+        Me.btnreplay.Name = "btnreplay"
+        Me.btnreplay.Size = New System.Drawing.Size(203, 56)
+        Me.btnreplay.TabIndex = 0
+        Me.btnreplay.Text = "Replay"
+        Me.btnreplay.UseVisualStyleBackColor = True
+        Me.btnreplay.Visible = False
+        '
+        'lblnext
+        '
+        Me.lblnext.AutoSize = True
+        Me.lblnext.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnext.ForeColor = System.Drawing.Color.White
+        Me.lblnext.Location = New System.Drawing.Point(561, 34)
+        Me.lblnext.Name = "lblnext"
+        Me.lblnext.Size = New System.Drawing.Size(126, 25)
+        Me.lblnext.TabIndex = 2
+        Me.lblnext.Text = "Next Piece"
+        Me.lblnext.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblcredit
+        '
+        Me.lblcredit.AutoSize = True
+        Me.lblcredit.BackColor = System.Drawing.Color.Black
+        Me.lblcredit.ForeColor = System.Drawing.Color.White
+        Me.lblcredit.Location = New System.Drawing.Point(269, 357)
+        Me.lblcredit.Name = "lblcredit"
+        Me.lblcredit.Size = New System.Drawing.Size(197, 13)
+        Me.lblcredit.TabIndex = 4
+        Me.lblcredit.Text = "A simple game of tetris by Jason Graham"
+        Me.lblcredit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnstart
+        '
+        Me.btnstart.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnstart.Location = New System.Drawing.Point(202, 488)
+        Me.btnstart.Name = "btnstart"
+        Me.btnstart.Size = New System.Drawing.Size(330, 172)
+        Me.btnstart.TabIndex = 5
+        Me.btnstart.Text = "START GAME"
+        Me.btnstart.UseVisualStyleBackColor = True
+        '
+        'pcttitle
+        '
+        Me.pcttitle.Image = Global.Tetris.My.Resources.Resources.tetris_logo_1027x780
+        Me.pcttitle.InitialImage = Global.Tetris.My.Resources.Resources.tetris_logo_1027x780
+        Me.pcttitle.Location = New System.Drawing.Point(43, 20)
+        Me.pcttitle.Name = "pcttitle"
+        Me.pcttitle.Size = New System.Drawing.Size(679, 309)
+        Me.pcttitle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pcttitle.TabIndex = 3
+        Me.pcttitle.TabStop = False
+        '
+        'pctnext
+        '
+        Me.pctnext.Location = New System.Drawing.Point(545, 62)
+        Me.pctnext.Name = "pctnext"
+        Me.pctnext.Size = New System.Drawing.Size(158, 120)
+        Me.pctnext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pctnext.TabIndex = 1
+        Me.pctnext.TabStop = False
+        '
+        'lblscore
+        '
+        Me.lblscore.AutoSize = True
+        Me.lblscore.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblscore.ForeColor = System.Drawing.Color.White
+        Me.lblscore.Location = New System.Drawing.Point(555, 217)
+        Me.lblscore.Name = "lblscore"
+        Me.lblscore.Size = New System.Drawing.Size(139, 25)
+        Me.lblscore.TabIndex = 6
+        Me.lblscore.Text = "Score: 0000"
+        Me.lblscore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(734, 981)
+        Me.Controls.Add(Me.btnstart)
+        Me.Controls.Add(Me.lblcredit)
+        Me.Controls.Add(Me.pcttitle)
+        Me.Controls.Add(Me.lblnext)
+        Me.Controls.Add(Me.pctnext)
+        Me.Controls.Add(Me.btnreplay)
+        Me.Controls.Add(Me.lblscore)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Tetris"
+        CType(Me.pcttitle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pctnext, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tmrpiece As System.Windows.Forms.Timer
+    Friend WithEvents tmrfastdown As System.Windows.Forms.Timer
+    Friend WithEvents btnreplay As System.Windows.Forms.Button
+    Friend WithEvents pctnext As System.Windows.Forms.PictureBox
+    Friend WithEvents lblnext As System.Windows.Forms.Label
+    Friend WithEvents pcttitle As System.Windows.Forms.PictureBox
+    Friend WithEvents lblcredit As System.Windows.Forms.Label
+    Friend WithEvents btnstart As System.Windows.Forms.Button
+    Friend WithEvents lblscore As System.Windows.Forms.Label
+
+End Class
---
+++ Tetris/Form1.vb
@@ -0,0 +1,478 @@
+﻿'Jason Graham
+'1/6/2020
+'A simple game of Tetris
+Public Class Form1
+    Const ROW As Byte = 20
+    Const COL As Byte = 11
+
+    Dim rand As New Random
+
+    Dim board(ROW, COL) As aSquare
+
+    Dim currentPiece(3) As aPiece
+    Dim bytpiece As Byte
+    Dim bytnext As Byte
+    Dim sturncount As Byte
+
+    Dim blnendturn As Boolean
+    Dim blngameover As Boolean = True
+
+    Dim bytturncount As Byte
+    Dim bytrotation As Byte
+
+    Dim shtscore As Short
+
+    Structure aSquare
+        Dim b As Rectangle
+        Dim c As Byte
+    End Structure
+
+    Structure aPiece
+        Dim l As Point
+        Dim n As Boolean
+        Dim color As Byte
+    End Structure
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If blngameover = False Then
+            If e.KeyCode = Keys.Left Then moveHorizontal(-1)
+            If e.KeyCode = Keys.Right Then moveHorizontal(1)
+
+            If e.KeyCode = Keys.Down Then
+                tmrpiece.Enabled = False
+                tmrfastdown.Enabled = True
+
+            End If
+
+            If e.KeyCode = Keys.Up Then
+                Turn()
+            End If
+
+            Me.Refresh()
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If blngameover = False Then
+            If e.KeyCode = Keys.Down Then
+                tmrfastdown.Enabled = False
+                tmrpiece.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub moveHorizontal(ByRef bytdirect As SByte)
+        For i = 0 To currentPiece.Length - 1
+            currentPiece(i).n = False
+
+            For u = 0 To currentPiece.Length - 1
+                If currentPiece(i).l.X = currentPiece(u).l.X And currentPiece(i).l.Y + bytdirect = currentPiece(u).l.Y Then currentPiece(i).n = True
+            Next
+        Next
+
+        For i = 0 To currentPiece.Length - 1
+            If currentPiece(i).n = False Then
+                If board(currentPiece(i).l.X, currentPiece(i).l.Y + bytdirect).c <> 0 Then Exit Sub
+            End If
+        Next
+
+        For i = 0 To currentPiece.Length - 1
+            board(currentPiece(i).l.X, currentPiece(i).l.Y).c = 0
+        Next
+
+        For i = 0 To currentPiece.Length - 1
+
+            currentPiece(i).l = New Point(currentPiece(i).l.X, currentPiece(i).l.Y + bytdirect)
+
+            board(currentPiece(i).l.X, currentPiece(i).l.Y).c = currentPiece(i).color
+
+        Next
+
+    End Sub
+
+    Private Sub Start()
+        Me.DoubleBuffered = True
+        tmrpiece.Enabled = True
+        blngameover = False
+        Me.Focus()
+
+        Dim xspot, yspot As Short
+        xspot = 20
+        yspot = 20
+
+        'make board
+        For r = 0 To ROW
+            For c = 0 To COL
+                board(r, c).b = New Rectangle(xspot, yspot, 40, 40)
+                xspot += 45
+
+            Next
+            yspot += 45
+            xspot = 20
+        Next
+
+        For i = 0 To COL
+            board(20, i).c = 1
+        Next
+
+        For i = 0 To ROW - 1
+            board(i, 0).c = 1
+            board(i, COL).c = 1
+        Next
+
+        bytnext = rand.Next(0, 7)
+
+        getPiece()
+    End Sub
+
+    Private Sub showNext()
+        If bytnext = 0 Then
+            pctnext.Image = My.Resources.T
+
+        ElseIf bytnext = 1 Then
+            pctnext.Image = My.Resources.straight
+
+        ElseIf bytnext = 2 Then
+            pctnext.Image = My.Resources.Square_svg
+
+        ElseIf bytnext = 3 Then
+            pctnext.Image = My.Resources.J
+
+        ElseIf bytnext = 4 Then
+            pctnext.Image = My.Resources.L_svg
+
+        ElseIf bytnext = 5 Then
+            pctnext.Image = My.Resources.Z
+
+        Else
+            pctnext.Image = My.Resources.S
+
+        End If
+    End Sub
+
+    Private Sub getPiece()
+        Dim bytnew As Byte = bytnext
+        Dim bytexclude As Byte = 10
+
+        bytturncount = 0
+
+        If bytpiece = bytnext Then
+            bytexclude = bytpiece
+
+        End If
+
+        bytpiece = bytnext
+
+        Do
+            bytnext = rand.Next(0, 7)
+        Loop While bytexclude = bytnext
+
+        showNext()
+
+        If bytpiece = 0 Then 'T Block
+            currentPiece(0).l = New Point(0, 5)
+            currentPiece(1).l = New Point(1, 5)
+            currentPiece(2).l = New Point(2, 5)
+            currentPiece(3).l = New Point(1, 6)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 1
+            Next
+
+        ElseIf bytpiece = 1 Then 'I Block
+            currentPiece(0).l = New Point(0, 5)
+            currentPiece(1).l = New Point(1, 5)
+            currentPiece(2).l = New Point(2, 5)
+            currentPiece(3).l = New Point(3, 5)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 2
+            Next
+
+        ElseIf bytpiece = 2 Then 'Square
+            currentPiece(0).l = New Point(0, 5)
+            currentPiece(1).l = New Point(0, 6)
+            currentPiece(2).l = New Point(1, 5)
+            currentPiece(3).l = New Point(1, 6)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 3
+            Next
+
+        ElseIf bytpiece = 3 Then 'J Block
+            currentPiece(0).l = New Point(0, 6)
+            currentPiece(1).l = New Point(1, 6)
+            currentPiece(2).l = New Point(2, 6)
+            currentPiece(3).l = New Point(2, 5)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 4
+            Next
+
+        ElseIf bytpiece = 4 Then 'L Block
+            currentPiece(0).l = New Point(0, 5)
+            currentPiece(1).l = New Point(1, 5)
+            currentPiece(2).l = New Point(2, 5)
+            currentPiece(3).l = New Point(2, 6)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 5
+            Next
+
+        ElseIf bytpiece = 5 Then 'Z Block
+            currentPiece(0).l = New Point(0, 6)
+            currentPiece(1).l = New Point(1, 6)
+            currentPiece(2).l = New Point(1, 5)
+            currentPiece(3).l = New Point(2, 5)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 6
+            Next
+
+        Else 'S Block
+            currentPiece(0).l = New Point(0, 5)
+            currentPiece(1).l = New Point(1, 5)
+            currentPiece(2).l = New Point(1, 6)
+            currentPiece(3).l = New Point(2, 6)
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).color = 7
+            Next
+
+        End If
+
+
+
+        For i = 0 To currentPiece.Length - 1
+            board(currentPiece(i).l.X, currentPiece(i).l.Y).c = currentPiece(i).color
+        Next
+
+
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        For r = 0 To ROW - 1
+            For c = 1 To COL - 1
+                If board(r, c).c = 0 Then
+                    e.Graphics.FillRectangle(Brushes.Gray, board(r, c).b)
+                ElseIf board(r, c).c = 1 Then
+                    e.Graphics.FillRectangle(Brushes.Purple, board(r, c).b)
+                ElseIf board(r, c).c = 2 Then
+                    e.Graphics.FillRectangle(Brushes.Blue, board(r, c).b)
+                ElseIf board(r, c).c = 3 Then
+                    e.Graphics.FillRectangle(Brushes.Yellow, board(r, c).b)
+                ElseIf board(r, c).c = 4 Then
+                    e.Graphics.FillRectangle(Brushes.DarkBlue, board(r, c).b)
+                ElseIf board(r, c).c = 5 Then
+                    e.Graphics.FillRectangle(Brushes.Orange, board(r, c).b)
+                ElseIf board(r, c).c = 6 Then
+                    e.Graphics.FillRectangle(Brushes.Red, board(r, c).b)
+                Else
+                    e.Graphics.FillRectangle(Brushes.Lime, board(r, c).b)
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub tmrpiece_Tick(sender As Object, e As EventArgs) Handles tmrpiece.Tick
+
+        Down()
+
+        Me.Refresh()
+    End Sub
+
+    Private Sub Down()
+        Dim bytconsecutiverows As Byte
+
+        bytturncount += 1
+
+        blnendturn = False
+
+        'Move the pieces down
+        For i = 0 To currentPiece.Length - 1
+            currentPiece(i).n = False
+        Next
+
+        For j = 0 To currentPiece.Length - 1
+            For k = 0 To currentPiece.Length - 1
+                If currentPiece(j).l.X + 1 = currentPiece(k).l.X And currentPiece(j).l.Y = currentPiece(k).l.Y Then
+                    currentPiece(j).n = True
+                End If
+            Next
+        Next
+
+        For i = 0 To currentPiece.Length - 1
+            If currentPiece(i).n = False Then
+                If board(currentPiece(i).l.X + 1, currentPiece(i).l.Y).c <> 0 Then
+                    blnendturn = True
+                    Exit For
+                End If
+            End If
+        Next
+
+        If blnendturn = False Then
+            For i = 0 To currentPiece.Length - 1
+                board(currentPiece(i).l.X, currentPiece(i).l.Y).c = 0
+
+            Next
+
+            For i = 0 To currentPiece.Length - 1
+                currentPiece(i).l = New Point(currentPiece(i).l.X + 1, currentPiece(i).l.Y)
+
+                board(currentPiece(i).l.X, currentPiece(i).l.Y).c = currentPiece(i).color
+            Next
+
+        Else
+            If bytturncount = 1 Then
+                Lose()
+
+            End If
+            'Check for complete line
+            For r = ROW - 1 To 0 Step -1
+same_line:      For c = 1 To COL - 1
+                    If board(r, c).c = 0 Then
+                        GoTo next_line
+                    End If
+                Next
+
+                For c = 1 To COL - 1
+                    board(r, c).c = 0
+
+                Next
+
+                For m = r - 1 To 0 Step -1
+                    For c = 1 To COL - 1
+                        If board(m, c).c <> 0 Then
+                            board(m + 1, c).c = board(m, c).c
+                            board(m, c).c = 0
+                        End If
+                    Next
+
+                Next
+
+                For i = 0 To currentPiece.Length - 1
+                    currentPiece(i).l = New Point(currentPiece(i).l.X + 1, currentPiece(i).l.Y)
+                Next
+
+                bytconsecutiverows += 1
+
+                GoTo same_line
+next_line:  Next
+
+            If bytconsecutiverows > 0 Then
+                If bytconsecutiverows = 1 Or bytconsecutiverows = 2 Then
+                    shtscore += 100 * bytconsecutiverows
+
+                ElseIf bytconsecutiverows = 3 Then
+                    shtscore += 400
+
+                Else
+                    shtscore += 800
+
+                End If
+
+                If shtscore >= 1000 Then
+                    lblscore.Text = "Score: " & shtscore
+
+                Else
+                    lblscore.Text = "Score: 0" & shtscore
+
+                End If
+
+            End If
+
+            getPiece()
+        End If
+    End Sub
+
+    Private Sub Turn()
+        If bytpiece <> 2 Then
+
+            Dim oldpos As Point = currentPiece(1).l
+            Dim dist As Point
+            Dim newpoint(currentPiece.Length - 1) As Point
+
+            For i = 0 To currentPiece.Length - 1
+                newpoint(i) = currentPiece(i).l
+                newpoint(i) = New Point(newpoint(i).Y, -newpoint(i).X)
+            Next
+
+            dist = New Point(newpoint(1).X - oldpos.X, newpoint(1).Y - oldpos.Y)
+
+            For i = 0 To currentPiece.Length - 1
+                newpoint(i) = New Point(newpoint(i).X - dist.X, newpoint(i).Y - dist.Y)
+            Next
+
+            For i = 0 To newpoint.Length - 1
+                'Check if turning puts shape out of bounds
+                If newpoint(i).Y < 1 Or newpoint(i).Y > COL - 1 Or newpoint(i).X < 1 Or newpoint(i).X > ROW - 1 Then Exit Sub
+
+                If board(newpoint(i).X, newpoint(i).Y).c <> 0 Then
+                    For j = 0 To currentPiece.Length - 1
+                        If newpoint(i) = currentPiece(j).l Then GoTo valid
+
+                    Next
+                    Exit Sub
+                End If
+valid:      Next
+
+            For i = 0 To currentPiece.Length - 1
+                board(currentPiece(i).l.X, currentPiece(i).l.Y).c = 0
+                currentPiece(i).l = newpoint(i)
+            Next
+
+            For i = 0 To currentPiece.Length - 1
+                board(currentPiece(i).l.X, currentPiece(i).l.Y).c = currentPiece(i).color
+
+            Next
+
+            Me.Refresh()
+
+        End If
+
+
+    End Sub
+
+    Private Sub Lose()
+        tmrfastdown.Enabled = False
+        tmrpiece.Enabled = False
+        blngameover = True
+
+        btnreplay.Visible = True
+
+    End Sub
+
+    Private Sub tmrfastdown_Tick(sender As Object, e As EventArgs) Handles tmrfastdown.Tick
+        Down()
+        Me.Refresh()
+    End Sub
+
+    Private Sub btnreplay_Click(sender As Object, e As EventArgs) Handles btnreplay.Click
+        btnreplay.Visible = False
+
+        shtscore = 0
+
+        For r = ROW - 1 To 0 Step -1
+            For c = 1 To COL - 1
+                board(r, c).c = 0
+            Next
+        Next
+
+        blngameover = False
+
+        getPiece()
+        tmrpiece.Enabled = True
+
+        Me.Refresh()
+        Me.Focus()
+    End Sub
+
+    Private Sub btnstart_Click(sender As Object, e As EventArgs) Handles btnstart.Click
+        pcttitle.Visible = False
+        lblcredit.Visible = False
+        btnstart.Visible = False
+
+        Start()
+        Me.Refresh()
+    End Sub
+End Class
